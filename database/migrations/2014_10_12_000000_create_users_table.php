e6bfc5a5c735978cd42d58d9a85ca650a03ff0e1
jccintr <jccintr@gmail.com>
2023-02-14 18:32:26 -0400

    alterado user migration campos

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,19 +15,19 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name',60);
-            $table->string('email',50)->unique();
+            $table->string('name',100);
+            $table->string('email',100)->unique();
             $table->string('phone',20)->nullable();
             $table->string('doc',20)->nullable();
             $table->string('address',100)->nullable();
-            $table->string('password',20);
+            $table->string('password');
            // $table->integer('role')->default(2);  // 0-admin 1-gerente 2-funcionario
             $table->boolean('active')->default(true);
             $table->boolean('isAdmin')->default(false);
-            $table->string('token',50)->nullable();
+            $table->string('token')->nullable();
             $table->timestamp('token_expire_at')->nullable();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('avatar',80)->nullable();
+            $table->string('avatar')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
