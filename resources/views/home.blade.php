b63fb0290e951acb1ca83da178d4b10dcffcc0db
jccintr <jccintr@gmail.com>
2023-02-26 18:35:37 -0400

    alterado home view

---
+++ resources/views/home.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<style>
+body {
+    background-color:  #000a16;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    min-height: 100vh;
+}
+
+a {
+    text-decoration: none;
+    color: white;
+    margin-top: 50px;
+}
+</style>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Absentor</title>
+</head>
+<body>
+   
+    <img src="{{url('logo-absentor-310x220.png')}}" alt="Absentor Logo">
+    <a href="http://jc-software.surge.sh">Desenvolvido por JC Software</a>
+   
+</body>
+</html>
